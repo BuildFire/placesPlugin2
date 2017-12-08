ccff71e1f2b26d35d069d962f48300197ba735a9
AlexKvazos <contact@alexkvazos.com>
2017-12-07 18:07:12 -0600

    Match detail view to spec

--- src/widget/js/detail.js
+++ src/widget/js/detail.js
@@ -1,4 +1,4 @@
-import Handlebars from "./lib/handlebars"
+import Handlebars from './lib/handlebars';
 
 window.detailView = {
     init: (place) => {
@@ -9,7 +9,7 @@ window.detailView = {
         const imageName = place.image ? place.image : 'holder-16x9.png';
 
         //TODO:This is hacky ... come up with a better way
-        const mapHeight = document.getElementById('view').getBoundingClientRect().height/3;
+        const mapHeight = 238;
 
         let context = {
             width: screenWidth,
@@ -37,8 +37,7 @@ window.detailView = {
                 view.innerHTML = theCompiledHtml;
 
                 //TODO: Move to common location
-                let mapTypeId = google.maps.MapTypeId.ROADMAP,
-                    zoomPosition = google.maps.ControlPosition.RIGHT_TOP,
+                let mapTypeId = window.google.maps.MapTypeId.ROADMAP,
                     zoomTo = 14, //city
                     centerOn = {lat: place.address.lat, lng: place.address.lng};
 
@@ -49,25 +48,23 @@ window.detailView = {
                     zoom: zoomTo,
                     center: centerOn,
                     mapTypeId: mapTypeId,
-                    zoomControlOptions: {
-                        position: zoomPosition
-                    }
+                    disableDefaultUI: true
                 };
 
-                map = new google.maps.Map(document.getElementById('smallMap'), options);
+                let map = new window.google.maps.Map(document.getElementById('smallMap'), options);
 
                 const iconBaseUrl = 'https://app.buildfire.com/app/media/',
                     icon = {
                         url: iconBaseUrl + 'google_marker_green_icon.png',
                         // This marker is 20 pixels wide by 20 pixels high.
-                        scaledSize: new google.maps.Size(20, 20),
+                        scaledSize: new window.google.maps.Size(20, 20),
                         // The origin for this image is (0, 0).
-                        origin: new google.maps.Point(0, 0),
+                        origin: new window.google.maps.Point(0, 0),
                         // The anchor for this image is at the center of the circle
-                        anchor: new google.maps.Point(10, 10)
-                    }
+                        anchor: new window.google.maps.Point(10, 10)
+                    };
 
-                new google.maps.Marker({
+                new window.google.maps.Marker({
                     position: place.address,
                     map,
                     icon
