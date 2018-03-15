272b021c68e26e6f6e225bf3d7494682de8ecc59
Alejandro Cavazos <contact@alexkvazos.com>
2018-03-15 16:29:50 -0600

    Keep original name

--- src/control/content/components/LocationForm.js
+++ src/control/content/components/LocationForm.js
@@ -245,7 +245,7 @@ class LocationForm extends React.Component {
         [{ color: [] }, { background: [] }],
         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
         [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-        ['link', 'image', 'video', 'code']
+        ['link', 'image', 'video']
       ],
       handlers: {
         image: this.imageHandler.bind(this)
